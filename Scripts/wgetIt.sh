638f7ad130750c160262fe7d9583f69be82b93c2
bn2645@mun.ca <benxinniu880808>
2018-04-02 12:18:26 -0230

    implemented new features

--- Scripts/wgetIt.sh
+++ Scripts/wgetIt.sh
@@ -1,20 +1,103 @@
-#bash/bin
-name = "${1}"
-workingDir= "${2}"
+#!/bin/bash
+
+option=${1}
+
+
+declare -a arr=("AAPL" "AMZN" "BABA" "GOOGL" "GOOS" "AMD" "INTC" "BA" "BABY" "AZDDQ" "BLVD" "BMBM" "BMGP" "CCGY" "CFED" "CHMO" "CHNR" "CHOR" "CHPC"
+                 "CIVS" "CLWT" "CMFO" "CNVT" "CRGE" "CUI" "CVST" "CYE" "DRGG" "IMDZ" "INNO" "INST" "ITUS" "KERX" "KLOC" "LADR" "LATX" "LEHLQ")
+
+for i in "${arr[@]}"
+
+do
+
+echo "processing" "$i"
+
+if [ ! -d /home/benxin/stockopedia_daily/${i} ]; then
+
+mkdir -p /home/benxin/stockopedia_daily/${i}
+
+fi;
 
 
 
- wget --user 0f731094cd8305b1859398def068acba --password d4589289cabd7f987f85861a3f55c787 https://api.intrinio.com/prices.csv?identifier=${name} -O /home/benxin/tmp/=prices.csv  \
+wget --user 0f731094cd8305b1859398def068acba --password d4589289cabd7f987f85861a3f55c787 https://api.intrinio.com/prices.csv?identifier="$i" -O /home/benxin/stockopedia_daily/${i}/price.csv  \
 
- wget --user 0f731094cd8305b1859398def068acba --password d4589289cabd7f987f85861a3f55c787 https://api.intrinio.com/options.csv?identifier==${name} -O /home/benxin/tmp/=options.csv  \ 
 
- wget --user 0f731094cd8305b1859398def068acba --password d4589289cabd7f987f85861a3f55c787 https://api.intrinio.com/news.csv?identifier==${name} -O /home/benxin/tmp/news.csv  \ 
 
- wget --user 0f731094cd8305b1859398def068acba --password d4589289cabd7f987f85861a3f55c787 https://api.intrinio.com/companies.csv?identifier==${name} -O /home/benxin/tmp/companies.csv  \ 
+if [ -f  /home/benxin/stockopedia_daily/${i}/price.csv ]; then
 
- wget --user 0f731094cd8305b1859398def068acba --password d4589289cabd7f987f85861a3f55c787 https://api.intrinio.com/owners.csv?identifier=AAPL -O /home/benxin/tmp/owners.csv  \
+echo "removing the first line" \
 
-wget --user 0f731094cd8305b1859398def068acba --password d4589289cabd7f987f85861a3f55c787 https://api.intrinio.com/financials/reported.csv?identifier=${name}&statement=income_statement&fiscal_year=2015&fiscal_period=FY -O /home/benxin/tmp/reportedFinancial.csv  \
+sed -i '1d' /home/benxin/stockopedia_daily/${i}/price.csv  \
+
+else
+
+echo "no file downloded" \
+
+fi;
+
+ sleep 7s  \
+
+
+if [ ${option} -eq true ];
+
+then
+
+ wget --user 0f731094cd8305b1859398def068acba --password d4589289cabd7f987f85861a3f55c787 https://api.intrinio.com/options.csv?ticker="$i" -O /home/benxin/stockopedia_daily/${i}/options.csv  \
+
+if [ -f  /home/benxin/stockopedia_daily/${i}/options.csv ]; then
+
+echo "removing the first line" \
+
+sed -i '1d' /home/benxin/stockopedia_daily/${i}/options.csv  \
+
+else
+
+echo "no file downloded" \
+
+fi;
+
+ sleep 7s  \
+else
+
+echo "not downloading option for " ${i}
+
+fi;
+
+wget --user 0f731094cd8305b1859398def068acba --password d4589289cabd7f987f85861a3f55c787 https://api.intrinio.com/news.csv?identifier="$i" -O /home/benxin/stockopedia_daily/${i}/news.csv
+
+if [ -f  /home/benxin/stockopedia_daily/${i}/news.csv ]; then
+
+echo "removing the first line" \
+
+sed -i '1d' /home/benxin/stockopedia_daily/${i}/news.csv  \
+
+else
+
+echo "no file downloded" \
+
+fi;
+
+sleep 7s  \
+
+done
+
+sleep 7s  \
+
+#wget --user 0f731094cd8305b1859398def068acba --password d4589289cabd7f987f85861a3f55c787 https://api.intrinio.com/companies.csv -O /home/benxin/stockopedia_daily/inventory.csv
+
+if [ -f  /home/benxin/stockopedia_daily/inventory.csv ]; then
+
+echo "removing the first line" \
+
+#sed -i '1d' /home/benxin/stockopedia_daily/inventory.csv  \
+
+else
+
+echo "no file downloded" \
+
+fi;
+
 
 
 echo "success"
